c29c7478159328fb20a0d9072ee9b1caef30c401
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-28 23:51:04 +0300

    system_programming broadcast_pattern_template.c: identifying actual problems to solve in observers-broadcaster design pattern, in addition added 2 structs, one for observer that constains id, recent read msg version, and a handle to it's instance's thread, and a struct to message contains msg itself, the version of it and a flag is_observed thats on only when all observers read the message

--- system_programming/src/broadcast_pattern_template.c
+++ system_programming/src/broadcast_pattern_template.c
@@ -13,4 +13,39 @@
 	- one to many dependincies 
 */
 
+/*
+	problems to be solved:
+	1- no race consition: either observbers / broadcaster enter the critical
+	section (message)
+	2- notification: observers must know a new message has sent 
+	3- all observers read message simultainsly
+	4- avoid duplication: avoid same observer to read same message more than once
+	5- avoid busy waiting
+*/
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define OBSERVERS_NUM	3
+#define MSG_NUM			5
+
+/* observer's struct */
+typedef struct
+{
+    int id;			/*observer id (to print)*/
+    int recent_version;		/*marks recent version the observer has read*/
+    pthread_t observer_thrd;	/*handle for observer's thread*/
+} observer_ty;
+
+typedef struct
+{
+	int msg = 0;	/* critical resource*/
+	int msg_version = 0;	/*utilied to make sure observers do not read 
+							same message more than once */
+	int is_observed = 0;	/*this flag identifies to broadcaster that 
+							all observers has read the message and ite
+							can write a new messgae*/
+}msg_ty;
 
