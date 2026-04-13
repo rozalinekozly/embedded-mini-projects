152c08e3aa196c22ba93f6e279e7b86e3c4638c4
rozaline.kozly <rozaline.ko@gmail.com>
2026-04-13 15:25:13 +0300

    client_app.c : adding basic client app that uses wd (psudo code), prints its pid 2000 times

--- system_programming/src/client_app.c
+++ system_programming/src/client_app.c
@@ -1,4 +1,18 @@
 /*
 	client app that uses wd.h functions MakeMeImmortal and DoNotResussitate
 	version 1
-*/
+*/ 
+#include <stdio.h>		/*printf()*/
+#include <unistd.h> 	/* getpid, sleep */
+#include "wd.h"			/*watchdog*/
+
+int main(int argc, char* argv[])
+{
+	/*call MakeMeImmortal*/
+	/*check return value*/
+	/*if failed*/
+		/*exit*/
+	/*print PID 2000 times*/
+	/*call DoNotResussitate*/
+	return 0;
+}
