e5f110a745dfcbe0f265d9591d3da7a8c3f37139
rozaline.kozly <rozaline.ko@gmail.com>
2026-04-13 15:44:09 +0300

    wd.c : adding psudo code of MakeMeImmortal function, this psudo do not contain how to extract and organize arguments for wd_app execvp yet

--- system_programming/src/wd.c
+++ system_programming/src/wd.c
@@ -2,4 +2,28 @@
 	1- MakeMeImmortal 
 	2- DoNotResussitate
 	implementation
+int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often)
+void DoNotResussitate(void)
 */
+int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often)
+{
+	/*asserts*/
+	/*fork*/
+	/*if failed*/
+		/*return 1 (=failed)*/
+	
+	/*if child*/
+		/*replace code with wd_app with proper arguments via execvp*/
+		/*if execvp failed*/
+			/*exit*/
+	
+	/*if parent*/
+		/*version 1: sleep 2 seconds */
+	
+	/*return 0 = success*/
+}
+
+void DoNotResussitate(void)
+{
+
+}
