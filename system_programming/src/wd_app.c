3fc3d3972b2656e7870d8f32879c9c7180bd4b94
rozaline.kozly <rozaline.ko@gmail.com>
2026-04-13 05:57:27 +0300

    wd: adding the task psudo as ReviveIfNotAliveIMP, uses kill 0 to check if program still alive, aslo a dummy cleanup function to utilies scheduler's api

--- system_programming/src/wd_app.c
+++ system_programming/src/wd_app.c
@@ -1,12 +1,21 @@
 /*
 	watch dog app version 2: user input <wd_app_exe> <frequency_check> <client_app>
 */
+
 typedef struct
 {
 	pid_t pid;
 	char** client_cmd;
 	int frequency_check;
 }client_ty;
+
+#define UNUSED(x)	(void)x
+
+/*forward declarations*/
+static sch_op_status_ty ReviveIfNotAliveIMP(void* param);
+static void RevivalCleanupIMP(void* unused);
+
+
 
 int main(int argc, char* argv[])
 {
@@ -23,7 +32,8 @@ int main(int argc, char* argv[])
 	/*create a scheduler*/
 	/*handle failure*/
 	
-	/*add task to scheduler with frequency_check as an interval*/
+	/*add task to scheduler with frequency_check as an interval, op_func as ReviveIfNotAliveIMP,
+	op_param &client instance, clean_func = dummy cleanup func, clean_param is null*/
 	/*handle failure*/
 	
 	/*run scheduler*/
@@ -33,3 +43,24 @@ int main(int argc, char* argv[])
 	
 	return 0;
 }
+
+static sch_op_status_ty ReviveIfNotAliveIMP(void* param_)
+{
+	/*cast param to client_ty*/
+
+	/*send client app instance a signal via kill 0*/
+	/*if fails (client is dead)*/
+		/*execvp(client_cmd[0], client_cmd)*/
+		/*execvp failed*/
+			/*return SCH_NOT_REPEAT*/
+
+	/*client alive */
+		/*return SCH_REPEAT*/
+}
+
+static void RevivalCleanupIMP(void* param_)
+{
+	/*do nothing*/
+	UNUSED(param_);
+}
+
