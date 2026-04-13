62a5a7bf0ad6630c4ee9178676e1285e7ed13b85
rozaline.kozly <rozaline.ko@gmail.com>
2026-04-13 15:30:13 +0300

    wd.h : header file for watchdog shared library, it includes signatures of the 2 functions (fw declaration)

--- system_programming/include/wd.h
+++ system_programming/include/wd.h
@@ -1,3 +1,15 @@
 /*
 	header file for shared library wd.so
 */
+/*
+	return values: 0 = success
+				   1 = failed
+*/
+#ifndef ILRD__WD_H__
+#define ILRD__WD_H__
+
+int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often);
+
+void DoNotResussitate(void);
+
+#endif
