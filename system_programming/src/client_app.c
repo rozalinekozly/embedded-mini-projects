4505ce254aec03cb31d1dd6ec543a4934c0fd639
rozaline.kozly <rozaline.ko@gmail.com>
2026-04-14 05:11:05 +0300

    client_app.c : implementation of psudo-code, added enum to allow changing numbers easily and prevent magic numbers, client app sleeps 1 each printing to allow manually killing the process to actually see iand test my wd.c

--- system_programming/src/client_app.c
+++ system_programming/src/client_app.c
@@ -4,15 +4,34 @@
 */ 
 #include <stdio.h>		/*printf()*/
 #include <unistd.h> 	/* getpid, sleep */
-#include "wd.h"			/*watchdog*/
-
+/*-----------------------------------------------------------------------------*/
+#include "utils.h"		/*EXIT_IF_BAD*/
+#include "wd.h"			/*DoNotResussitate, MakeMeImmortal*/
+/*-----------------------------------------------------------------------------*/
+enum
+{
+	CLIENT_HOW_OFTEN = 2,
+	NUM_TO_PRINT = 2000
+};
+/*-----------------------------------------------------------------------------*/
 int main(int argc, char* argv[])
 {
+	 int i = 0;
+	 int status = 0;
+	 pid_t pid = getpid();
 	/*call MakeMeImmortal*/
+	status = MakeMeImmortal((argc), (const char**)argv, CLIENT_HOW_OFTEN);
 	/*check return value*/
-	/*if failed*/
+		/*if failed*/
 		/*exit*/
+	EXIT_IF_BAD(0 == status, 1, "failed to create wd ");
 	/*print PID 2000 times*/
+	for(i = 0 ; i < NUM_TO_PRINT ; i++)
+	{
+		printf("client app PID: %d\n", pid);
+		sleep(1);
+	}
 	/*call DoNotResussitate*/
+	DoNotResussitate();
 	return 0;
 }
