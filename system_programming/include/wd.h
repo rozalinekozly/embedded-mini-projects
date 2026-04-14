a9134c99529594d0a621f2e662f26c4b5d233d01
rozaline.kozly <rozaline.ko@gmail.com>
2026-04-14 06:18:50 +0300

    wd.h: added enum for success and failure

--- system_programming/include/wd.h
+++ system_programming/include/wd.h
@@ -1,13 +1,20 @@
 /*
 	header file for shared library wd.so
 */
+
+#ifndef ILRD__WD_H__
+#define ILRD__WD_H__
+
+enum
+{
+	SUCCESS = 0,
+	FAIL = 1
+};
+
 /*
 	return values: 0 = success
 				   1 = failed
 */
-#ifndef ILRD__WD_H__
-#define ILRD__WD_H__
-
 int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often);
 
 void DoNotResussitate(void);
