4c68737678dbdfe2111b01944610628815cf6ad6
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-04-26 01:05:21 +0300

    wd: adding intial psudo code for final version, using FSM transition table, and change mode funciton

--- system_programming/src/wd.c
+++ system_programming/src/wd.c
@@ -1,96 +1,353 @@
-/* shared library of watchdog that contains 
-	implementation
-int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often)
-void DoNotResussitate(void)
-*/
-#include <stdlib.h>		/*malloc, free, exit*/
-#include <assert.h>		/*assert*/
-#include <unistd.h>		/* fork, execvp, sleep */
-#include <stdio.h>		/* sprintf */
-#include <signal.h>		/* kill, SIGTERM */
-#include <semaphore.h> /*sem_t, sem_init, sem_wait, sem_post, sem_destroy */
-#include <sys/wait.h>	/* waitpid */
+#define _POSIX_C_SOURCE 200112L
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
 #include <semaphore.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <time.h>
-/*----------------------------------------------------------------------------*/
+#include <pthread.h>
+#include <errno.h>
 #include "utils.h"
 #include "wd.h"
-/*----------------------------------------------------------------------------*/
-static pid_t g_wd_pid = 0;
-/*-----------------------------------------------------------------------------*/
+#include "scheduler.h"
+#include "uid.h"
+
+#define WD_APP      "./wd_app"
+#define WD_SEM_NAME "/wd_sem"
+#define UNUSED(x)   (void)x
+
+typedef enum
+{
+    MODE_INIT = 0,
+    MODE_ROUTINE,
+    MODE_REVIVE,
+    MODE_ERROR,
+    MODE_EXIT,
+    NUM_MODES
+} mode_ty;
+
 enum
 {
-	MAX_DIGITS = 20,
+    MAX_DIGITS = 20,
+    SEM_TIMEOUT_SEC = 5,
+    WD_APP_IDX      = 0,
+    INTERVAL_IDX    = 1,
+    FAIL_CNT_IDX    = 2,
+    CLIENT_ARGS_IDX = 3
 };
 
-/*-----------------------------------------------------------------------------*/
-static void FillWdArgsIMP(char** wd_args, const char** cmd, size_t cmd_len, char* how_often_str);
-/*-----------------------------------------------------------------------------*/
-int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often)
+typedef struct
 {
-	char** wd_args = NULL; 
-	char how_often_str[MAX_DIGITS] = {0};
-	sem_t* sem = sem_open("/wd_sem", O_CREAT, 0644, 0);
-	struct timespec ts;
-	
-	/*asserts*/
-	assert(how_often > 0);
-	assert(NULL != cmd);
-	
-	if (-1 == clock_gettime(CLOCK_REALTIME, &ts)) 
-	{
-		perror("clock_gettime");
-		return FAIL;
-	}
-	ts.tv_sec += 5; 
-	
-	/*build a wd_args strings array (char**) of size cmd_len+3*/
-	wd_args = (char**)malloc((cmd_len+3)*sizeof(char*));
-	/*handle failure*/
-	if(NULL == wd_args)
-	{
-		return FAIL;
-	}
-	
-	/*convert how_often to str*/
-    sprintf(how_often_str, "%d", how_often);
-	/*set wd_args enteries */
-		/*call FillWdArgsIMP*/
-	FillWdArgsIMP(wd_args, cmd, cmd_len, how_often_str);
-	
-	/*g_w_pid = fork*/
-	g_wd_pid = fork();
-	/*if failed*/
-	if(-1 == g_wd_pid)
-	{
-		/*cleanup*/
-		free(wd_args);
-		sem_close(sem);
-		/*return 1 (=failed)*/
-		return FAIL;
-	}
-	/*if child*/
-	if(0 == g_wd_pid)
-	{
+    char** m_client_argv;
+    size_t m_client_argv_len;
+    unsigned int m_interval;
+    unsigned int m_fail_cnt;
+    sem_t* m_sem;
+} wd_guard_info_ty;
 
-	}
-	/*if parent*/
-	else
-	{
+static pthread_t g_wd_guard_thread = 0;
 
-	}
-	
-	/*return 0 = success*/
-	return SUCCESS;
+static volatile unsigned int g_missed_signals_cnt = 0;
+static volatile int g_should_exit = 0;
+
+static void* WdGuardThreadIMP(void* wd_info);
+static sch_op_status_ty WaitForWdAppTSK(void* info);
+static sch_op_status_ty SendHeartbeatTSK(void* info);
+static sch_op_status_ty IncrementCounterTSK(void* info);
+static sch_op_status_ty CheckCounterTSK(void* info);
+static sch_op_status_ty CheckExitFlagTSK(void* info);
+static sch_op_status_ty ReviveWdAppTSK(void* info);
+static sch_op_status_ty ExitTSK(void* info);
+static sch_op_status_ty StopSchedulerTSK(void* info);
+static void SwitchModeIMP(tsk_info_ty* info, mode_ty new_mode);
+static void LoadInitTasksIMP(tsk_info_ty* info);
+static void LoadRoutineTasksIMP(tsk_info_ty* info);
+static void LoadReviveTasksIMP(tsk_info_ty* info);
+static void LoadErrorTasksIMP(tsk_info_ty* info);
+static void LoadExitTasksIMP(tsk_info_ty* info);
+static void AddTaskIMP(tsk_info_ty* info, scheduler_op_ty tsk_func);
+static char** AllocWdArgvIMP(wd_guard_info_ty* wd_info);
+static void FreeWdArgvIMP(char** wd_argv);
+static pid_t SpawnWdAppIMP(char** wd_argv);
+static void RegisterSignalHandlersIMP(void);
+static void TaskCleanupIMP(void* unused);
+static void ResetCounterSH(int sig);
+static void SetExitFlagSH(int sig);
+
+typedef void (*load_tasks_func_ty)(tsk_info_ty*);
+static load_tasks_func_ty transition_table[NUM_MODES] =
+{
+    LoadInitTasksIMP,
+    LoadRoutineTasksIMP,
+    LoadReviveTasksIMP,
+    LoadErrorTasksIMP,
+    LoadExitTasksIMP
+};
+
+int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often, int fail_cnt)
+{
+    /*assert args*/
+    /*create named semaphore initialized to 0*/
+    /*if failed*/
+        /*return FAIL*/
+    /*set timeout for sem_timedwait*/
+    /*if failed*/
+        /*sem_close*/
+        /*sem_unlink*/
+        /*return FAIL*/
+    /*init wd_guard_info*/
+    /*pthread_create WdGuardThreadIMP*/
+    /*if failed*/
+        /*sem_close*/
+        /*sem_unlink*/
+        /*return FAIL*/
+    /*sem_timedwait*/
+    /*if failed*/
+        /*pthread_cancel*/
+        /*pthread_join*/
+        /*sem_close*/
+        /*sem_unlink*/
+        /*return FAIL*/
+    /*sem_close*/
+    /*sem_unlink*/
+    /*store thread handle in g_wd_guard_thread*/
+    /*return SUCCESS*/
 }
-/*-----------------------------------------------------------------------------*/
-static void FillWdArgsIMP(char** wd_args, const char** cmd, size_t cmd_len, char* how_often_str)
-{
 
-/*-----------------------------------------------------------------------------*/
-void DoNotResussitate(void)
+void DoNotResuscitate(void)
 {
-
+    /*assert thread exists*/
+    /*set g_should_exit = 1 atomically*/
+    /*pthread_join*/
+    /*reset globals*/
 }
 
+static void* WdGuardThreadIMP(void* wd_info_)
+{
+    /*cast param*/
+    /*AllocWdArgvIMP*/
+    /*if failed*/
+        /*sem_post*/
+        /*return NULL*/
+    /*RegisterSignalHandlersIMP*/
+    /*SchedulerCreate*/
+    /*if failed*/
+        /*FreeWdArgvIMP*/
+        /*sem_post*/
+        /*return NULL*/
+    /*SpawnWdAppIMP*/
+    /*if failed*/
+        /*SchedulerDestroy*/
+        /*FreeWdArgvIMP*/
+        /*sem_post*/
+        /*return NULL*/
+    /*init tsk_info fields*/
+    /*SwitchModeIMP MODE_ROUTINE*/
+    /*SchedulerRun*/
+    /*SchedulerDestroy*/
+    /*FreeWdArgvIMP*/
+    /*return NULL*/
+}
+
+static void SwitchModeIMP(tsk_info_ty* info, mode_ty new_mode)
+{
+    /*assert info*/
+    /*assert new_mode < NUM_MODES*/
+    /*SchedulerClear*/
+    /*update m_current_mode*/
+    /*call transition_table[new_mode](info)*/
+}
+
+static sch_op_status_ty WaitForWdAppTSK(void* info_)
+{
+    /*cast param*/
+    /*assert sem exists*/
+    /*set timeout*/
+    /*sem_timedwait*/
+    /*if success*/
+        /*sem_post to unblock MMI*/
+        /*set m_sem = NULL*/
+        /*SwitchModeIMP MODE_ROUTINE*/
+    /*if timeout*/
+        /*sem_post to unblock MMI*/
+        /*set m_sem = NULL*/
+        /*SwitchModeIMP MODE_ERROR*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static sch_op_status_ty SendHeartbeatTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*kill(m_wd_app_pid, SIGUSR1)*/
+    /*if failed*/
+        /*if ESRCH*/
+            /*SwitchModeIMP MODE_REVIVE*/
+            /*return SCH_NOT_REPEAT*/
+        /*SwitchModeIMP MODE_ERROR*/
+        /*return SCH_NOT_REPEAT*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty IncrementCounterTSK(void* info_)
+{
+    /*atomic increment g_missed_signals_cnt*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty CheckCounterTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*if counter >= fail_cnt*/
+        /*SwitchModeIMP MODE_REVIVE*/
+        /*return SCH_NOT_REPEAT*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty CheckExitFlagTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*if g_should_exit*/
+        /*SwitchModeIMP MODE_EXIT*/
+        /*return SCH_NOT_REPEAT*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty ReviveWdAppTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*waitpid m_wd_app_pid*/
+    /*SpawnWdAppIMP*/
+    /*if failed*/
+        /*SwitchModeIMP MODE_ERROR*/
+        /*return SCH_NOT_REPEAT*/
+    /*update m_wd_app_pid*/
+    /*reset g_missed_signals_cnt = 0 atomically*/
+    /*SwitchModeIMP MODE_ROUTINE*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static sch_op_status_ty ExitTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*SchedulerStop*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static sch_op_status_ty StopSchedulerTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*kill(m_wd_app_pid, SIGUSR2)*/
+    /*waitpid m_wd_app_pid*/
+    /*SchedulerStop*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static void LoadInitTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP WaitForWdAppTSK*/
+}
+
+static void LoadRoutineTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP SendHeartbeatTSK*/
+    /*AddTaskIMP IncrementCounterTSK*/
+    /*AddTaskIMP CheckCounterTSK*/
+    /*AddTaskIMP CheckExitFlagTSK*/
+}
+
+static void LoadReviveTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP ReviveWdAppTSK*/
+}
+
+static void LoadErrorTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP ExitTSK*/
+}
+
+static void LoadExitTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP StopSchedulerTSK*/
+}
+
+static void AddTaskIMP(tsk_info_ty* info, scheduler_op_ty tsk_func)
+{
+    /*assert info*/
+    /*assert tsk_func*/
+    /*SchedulerAddTask with interval, tsk_func, info, TaskCleanupIMP*/
+    /*if failed*/
+        /*exit*/
+}
+
+static char** AllocWdArgvIMP(wd_guard_info_ty* wd_info)
+{
+    /*assert wd_info*/
+    /*malloc argv array of size client_argv_len + CLIENT_ARGS_IDX + 1*/
+    /*if failed return NULL*/
+    /*malloc interval_str*/
+    /*if failed*/
+        /*free wd_argv*/
+        /*return NULL*/
+    /*malloc fail_cnt_str*/
+    /*if failed*/
+        /*free interval_str*/
+        /*free wd_argv*/
+        /*return NULL*/
+    /*set wd_argv[0] = WD_APP*/
+    /*sprintf interval into interval_str*/
+    /*set wd_argv[1] = interval_str*/
+    /*sprintf fail_cnt into fail_cnt_str*/
+    /*set wd_argv[2] = fail_cnt_str*/
+    /*copy client_argv into wd_argv[3...]*/
+    /*set last entry to NULL*/
+    /*return wd_argv*/
+}
+
+static void FreeWdArgvIMP(char** wd_argv)
+{
+    /*if NULL return*/
+    /*free wd_argv[INTERVAL_IDX]*/
+    /*free wd_argv[FAIL_CNT_IDX]*/
+    /*free wd_argv*/
+}
+
+static pid_t SpawnWdAppIMP(char** wd_argv)
+{
+    /*fork*/
+    /*if failed return -1*/
+    /*if child*/
+        /*execvp wd_argv*/
+        /*exit 1*/
+    /*return pid*/
+}
+
+static void ResetCounterSH(int sig)
+{
+    /*atomic store g_missed_signals_cnt = 0*/
+}
+
+static void SetExitFlagSH(int sig)
+{
+    /*atomic store g_should_exit = 1*/
+}
+
+static void RegisterSignalHandlersIMP(void)
+{
+    /*sigemptyset*/
+    /*sigaction SIGUSR1 -> ResetCounterSH*/
+    /*sigaction SIGUSR2 -> SetExitFlagSH*/
+}
+
+static void TaskCleanupIMP(void* unused)
+{
+    /*UNUSED*/
+}
--- system_programming/src/wd_app.c
+++ system_programming/src/wd_app.c
@@ -1,35 +1,258 @@
-/*
-	watch dog app: user input <wd_app_exe> <frequency_check> <client_app>
-	This process is spawned by wd_thrd (a thread inside the client process).
-    Its job is to monitor the client process and revive it if it dies.
-    In return, wd_thrd monitors this process and revives it if it dies.
-    This creates a mutual protection system ie each side watches the other.
-*/
-/*TODO:
-- change scheduler to support for users to pass an empty function for cleanup(does nothing).
-- give perameters more meaningful names
-- add check if scheduler runningfailed SCH_FAIL
-- use getppid to check validity rather than kill 0 
-- errno of kill check it! 
-- */
-/*-----------------------------------------------------------------------------*/
-#define _POSIX_SOURCE
-#include <signal.h>		/* kill */
-#include <unistd.h>		/* getppid, execvp*/
-#include <stdlib.h>		/* atoi*/
-#include <assert.h>		/* assert  */
-#include <fcntl.h>
+#define _POSIX_C_SOURCE 199506L
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
 #include <semaphore.h>
-#include <stdio.h>		/*for printing remove later*/
-/*-----------------------------------------------------------------------------*/
+#include <fcntl.h>
+#include <time.h>
+#include <sys/wait.h>
 #include "utils.h"
 #include "scheduler.h"
-/*-----------------------------------------------------------------------------*/
+#include "uid.h"
+
+#define WD_SEM_NAME "/wd_sem"
+#define UNUSED(x) (void)x
+
+static pid_t g_client_pid = 0;
+
+typedef enum
+{
+    MODE_INIT = 0,
+    MODE_ROUTINE,
+    MODE_REVIVE,
+    MODE_ERROR,
+    MODE_EXIT,
+    NUM_MODES
+} mode_ty;
+
+enum
+{
+    INTERVAL_IDX = 1,
+    FAIL_CNT_IDX = 2,
+    CLIENT_ARGS_IDX = 3,
+    MIN_ARGS = 4,
+    SEM_TIMEOUT_SEC = 5
+};
+
 typedef struct
 {
-	pid_t m_pid;
-	char** m_client_cmd;
-	int m_frequency_check;
-}client_ty;
-/*-----------------------------------------------------------------------------*/
+    char** m_client_argv;
+    scheduler_ty* m_sch;
+    unsigned int m_fail_cnt;
+    unsigned int m_interval;
+    mode_ty m_current_mode;
+    sem_t* m_sem;
+} tsk_info_ty;
 
+static volatile unsigned int g_missed_signals_cnt = 0;
+static volatile int g_should_exit = 0;
+
+static sch_op_status_ty RaiseSemaphoreTSK(void* info);
+static sch_op_status_ty SendHeartbeatTSK(void* info);
+static sch_op_status_ty IncrementCounterTSK(void* info);
+static sch_op_status_ty CheckCounterTSK(void* info);
+static sch_op_status_ty CheckExitFlagTSK(void* info);
+static sch_op_status_ty ReviveTSK(void* info);
+static sch_op_status_ty ExitTSK(void* info);
+static sch_op_status_ty StopSchedulerTSK(void* info);
+static void SwitchModeIMP(tsk_info_ty* info, mode_ty new_mode);
+static void LoadInitTasksIMP(tsk_info_ty* info);
+static void LoadRoutineTasksIMP(tsk_info_ty* info);
+static void LoadReviveTasksIMP(tsk_info_ty* info);
+static void LoadErrorTasksIMP(tsk_info_ty* info);
+static void LoadExitTasksIMP(tsk_info_ty* info);
+static void AddTaskIMP(tsk_info_ty* info, scheduler_op_ty tsk_func);
+static void RegisterSignalHandlersIMP(void);
+static void TaskCleanupIMP(void* unused);
+static void ResetCounterSH(int sig);
+static void SetExitFlagSH(int sig);
+
+typedef void (*load_tasks_func_ty)(tsk_info_ty*);
+static load_tasks_func_ty transition_table[NUM_MODES] =
+{
+    LoadInitTasksIMP,
+    LoadRoutineTasksIMP,
+    LoadReviveTasksIMP,
+    LoadErrorTasksIMP,
+    LoadExitTasksIMP
+};
+
+int main(int argc, char* argv[])
+{
+    /*validate argc*/
+    /*g_client_pid = getppid()*/
+    /*parse interval*/
+    /*validate interval*/
+    /*parse fail_cnt*/
+    /*validate fail_cnt*/
+    /*set m_client_argv = argv + CLIENT_ARGS_IDX*/
+    /*RegisterSignalHandlersIMP*/
+    /*SchedulerCreate*/
+    /*if failed exit*/
+    /*set m_sch*/
+    /*SwitchModeIMP MODE_INIT*/
+    /*SchedulerRun*/
+    /*SchedulerDestroy*/
+    /*return 0*/
+}
+
+static void SwitchModeIMP(tsk_info_ty* info, mode_ty new_mode)
+{
+    /*assert info*/
+    /*assert new_mode < NUM_MODES*/
+    /*SchedulerClear*/
+    /*update m_current_mode*/
+    /*call transition_table[new_mode](info)*/
+}
+
+static sch_op_status_ty RaiseSemaphoreTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*sem_open WD_SEM_NAME*/
+    /*if failed*/
+        /*SwitchModeIMP MODE_ERROR*/
+        /*return SCH_NOT_REPEAT*/
+    /*sem_post*/
+    /*if failed*/
+        /*sem_close*/
+        /*SwitchModeIMP MODE_ERROR*/
+        /*return SCH_NOT_REPEAT*/
+    /*sem_close*/
+    /*SwitchModeIMP MODE_ROUTINE*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static sch_op_status_ty SendHeartbeatTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*kill(g_client_pid, SIGUSR1)*/
+    /*if failed*/
+        /*if ESRCH*/
+            /*SwitchModeIMP MODE_REVIVE*/
+            /*return SCH_NOT_REPEAT*/
+        /*SwitchModeIMP MODE_ERROR*/
+        /*return SCH_NOT_REPEAT*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty IncrementCounterTSK(void* info_)
+{
+    /*atomic increment g_missed_signals_cnt*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty CheckCounterTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*if counter >= fail_cnt*/
+        /*SwitchModeIMP MODE_REVIVE*/
+        /*return SCH_NOT_REPEAT*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty CheckExitFlagTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*if g_should_exit*/
+        /*SwitchModeIMP MODE_EXIT*/
+        /*return SCH_NOT_REPEAT*/
+    /*return SCH_REPEAT*/
+}
+
+static sch_op_status_ty ReviveTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*waitpid g_client_pid WNOHANG*/
+    /*fork*/
+    /*if failed*/
+        /*SwitchModeIMP MODE_ERROR*/
+        /*return SCH_NOT_REPEAT*/
+    /*if child*/
+        /*execvp client_argv*/
+        /*exit 1*/
+    /*update g_client_pid = pid*/
+    /*reset g_missed_signals_cnt = 0 atomically*/
+    /*SwitchModeIMP MODE_ROUTINE*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static sch_op_status_ty ExitTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*SchedulerStop*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static sch_op_status_ty StopSchedulerTSK(void* info_)
+{
+    /*cast param*/
+    /*assert param*/
+    /*SchedulerStop*/
+    /*return SCH_NOT_REPEAT*/
+}
+
+static void LoadInitTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP RaiseSemaphoreTSK*/
+}
+
+static void LoadRoutineTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP SendHeartbeatTSK*/
+    /*AddTaskIMP IncrementCounterTSK*/
+    /*AddTaskIMP CheckCounterTSK*/
+    /*AddTaskIMP CheckExitFlagTSK*/
+}
+
+static void LoadReviveTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP ReviveTSK*/
+}
+
+static void LoadErrorTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP ExitTSK*/
+}
+
+static void LoadExitTasksIMP(tsk_info_ty* info)
+{
+    /*AddTaskIMP StopSchedulerTSK*/
+}
+
+static void AddTaskIMP(tsk_info_ty* info, scheduler_op_ty tsk_func)
+{
+    /*assert info*/
+    /*assert tsk_func*/
+    /*SchedulerAddTask with interval tsk_func info TaskCleanupIMP*/
+    /*if failed exit*/
+}
+
+static void ResetCounterSH(int sig)
+{
+    /*atomic store g_missed_signals_cnt = 0*/
+}
+
+static void SetExitFlagSH(int sig)
+{
+    /*atomic store g_should_exit = 1*/
+}
+
+static void RegisterSignalHandlersIMP(void)
+{
+    /*sigemptyset*/
+    /*sigaction SIGUSR1 -> ResetCounterSH*/
+    /*sigaction SIGUSR2 -> SetExitFlagSH*/
+}
+
+static void TaskCleanupIMP(void* unused)
+{
+    /*UNUSED*/
+}
