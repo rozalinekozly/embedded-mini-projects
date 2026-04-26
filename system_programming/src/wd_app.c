a73ae8ece61fc4c4e5413827aaf45a4f1603493d
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-04-26 09:56:23 +0300

    wd_app.c : reorganising functions declarations, and starting implementing

--- system_programming/src/wd_app.c
+++ system_programming/src/wd_app.c
@@ -8,9 +8,9 @@
 #include <fcntl.h>
 #include <time.h>
 #include <sys/wait.h>
+
 #include "utils.h"
 #include "scheduler.h"
-#include "uid.h"
 
 #define WD_SEM_NAME "/wd_sem"
 #define UNUSED(x) (void)x
@@ -36,19 +36,41 @@ enum
     SEM_TIMEOUT_SEC = 5
 };
 
+/* management struct for a wd instance */
 typedef struct
 {
     char** m_client_argv;
     scheduler_ty* m_sch;
     unsigned int m_fail_cnt;
-    unsigned int m_interval;
+    unsigned int m_frequency_check;
     mode_ty m_current_mode;
     sem_t* m_sem;
-} tsk_info_ty;
+    pid_t m_partner_pid;
+} wd_management_ty;
+
+/*loaders function*/
+typedef void (*load_tasks_func_ty)(wd_management_ty*);
 
 static volatile unsigned int g_missed_signals_cnt = 0;
 static volatile int g_should_exit = 0;
 
+/*method to switch between modes*/
+static void SwitchModeIMP(wd_management_ty* info, mode_ty new_mode);
+/*loaders to init FSM */
+static void LoadInitTasksIMP(wd_management_ty* info);
+static void LoadRoutineTasksIMP(wd_management_ty* info);
+static void LoadReviveTasksIMP(wd_management_ty* info);
+static void LoadErrorTasksIMP(wd_management_ty* info);
+static void LoadExitTasksIMP(wd_management_ty* info);
+/*task of adding task to scheduler */
+/*wrapper but might delete later*/
+static void AddTaskIMP(wd_management_ty* info, scheduler_op_ty tsk_func);
+/*method to set signal handlers*/
+static void RegisterSignalHandlersIMP(void);
+/*signal handlers functions*/
+static void ResetCounterSigHandler(int sig);
+static void SetExitFlagSigHandler(int sig);
+/*tasks*/
 static sch_op_status_ty RaiseSemaphoreTSK(void* info);
 static sch_op_status_ty SendHeartbeatTSK(void* info);
 static sch_op_status_ty IncrementCounterTSK(void* info);
@@ -57,19 +79,10 @@ static sch_op_status_ty CheckExitFlagTSK(void* info);
 static sch_op_status_ty ReviveTSK(void* info);
 static sch_op_status_ty ExitTSK(void* info);
 static sch_op_status_ty StopSchedulerTSK(void* info);
-static void SwitchModeIMP(tsk_info_ty* info, mode_ty new_mode);
-static void LoadInitTasksIMP(tsk_info_ty* info);
-static void LoadRoutineTasksIMP(tsk_info_ty* info);
-static void LoadReviveTasksIMP(tsk_info_ty* info);
-static void LoadErrorTasksIMP(tsk_info_ty* info);
-static void LoadExitTasksIMP(tsk_info_ty* info);
-static void AddTaskIMP(tsk_info_ty* info, scheduler_op_ty tsk_func);
-static void RegisterSignalHandlersIMP(void);
+/*dummy funciton*/
 static void TaskCleanupIMP(void* unused);
-static void ResetCounterSH(int sig);
-static void SetExitFlagSH(int sig);
 
-typedef void (*load_tasks_func_ty)(tsk_info_ty*);
+/*transition table between modes*/
 static load_tasks_func_ty transition_table[NUM_MODES] =
 {
     LoadInitTasksIMP,
@@ -81,13 +94,20 @@ static load_tasks_func_ty transition_table[NUM_MODES] =
 
 int main(int argc, char* argv[])
 {
+	scheduler_ty* sch = NULL;
+	uid_ty task_id = {0};
+	wd_management_ty wd_management = {0};
+
     /*validate argc*/
-    /*g_client_pid = getppid()*/
-    /*parse interval*/
-    /*validate interval*/
-    /*parse fail_cnt*/
-    /*validate fail_cnt*/
-    /*set m_client_argv = argv + CLIENT_ARGS_IDX*/
+    EXIT_IF_BAD(3 <= argc, 1, "Invalid arguments");
+    
+    /*set wd management instance fields */
+    wd_management.m_frequency_check = atoi(argv[1]);
+    EXIT_IF_BAD(wd_management.m_frequency_check > 0, 1, "invaliud arguments");
+    wd_management.m_client_cmd = argv + CLIENT_ARGS_IDX;
+    wd_management.m_partner_pid = getppid();
+    wd_management.m_fail_cnt =  argv + FAIL_CNT_IDX;
+
     /*RegisterSignalHandlersIMP*/
     /*SchedulerCreate*/
     /*if failed exit*/
@@ -98,7 +118,7 @@ int main(int argc, char* argv[])
     /*return 0*/
 }
 
-static void SwitchModeIMP(tsk_info_ty* info, mode_ty new_mode)
+static void SwitchModeIMP(wd_management_ty* info, mode_ty new_mode)
 {
     /*assert info*/
     /*assert new_mode < NUM_MODES*/
@@ -199,12 +219,12 @@ static sch_op_status_ty StopSchedulerTSK(void* info_)
     /*return SCH_NOT_REPEAT*/
 }
 
-static void LoadInitTasksIMP(tsk_info_ty* info)
+static void LoadInitTasksIMP(wd_management_ty* info)
 {
     /*AddTaskIMP RaiseSemaphoreTSK*/
 }
 
-static void LoadRoutineTasksIMP(tsk_info_ty* info)
+static void LoadRoutineTasksIMP(wd_management_ty* info)
 {
     /*AddTaskIMP SendHeartbeatTSK*/
     /*AddTaskIMP IncrementCounterTSK*/
@@ -212,22 +232,22 @@ static void LoadRoutineTasksIMP(tsk_info_ty* info)
     /*AddTaskIMP CheckExitFlagTSK*/
 }
 
-static void LoadReviveTasksIMP(tsk_info_ty* info)
+static void LoadReviveTasksIMP(wd_management_ty* info)
 {
     /*AddTaskIMP ReviveTSK*/
 }
 
-static void LoadErrorTasksIMP(tsk_info_ty* info)
+static void LoadErrorTasksIMP(wd_management_ty* info)
 {
     /*AddTaskIMP ExitTSK*/
 }
 
-static void LoadExitTasksIMP(tsk_info_ty* info)
+static void LoadExitTasksIMP(wd_management_ty* info)
 {
     /*AddTaskIMP StopSchedulerTSK*/
 }
 
-static void AddTaskIMP(tsk_info_ty* info, scheduler_op_ty tsk_func)
+static void AddTaskIMP(wd_management_ty* info, scheduler_op_ty tsk_func)
 {
     /*assert info*/
     /*assert tsk_func*/
@@ -235,12 +255,12 @@ static void AddTaskIMP(tsk_info_ty* info, scheduler_op_ty tsk_func)
     /*if failed exit*/
 }
 
-static void ResetCounterSH(int sig)
+static void ResetCounterSigHandler(int sig)
 {
     /*atomic store g_missed_signals_cnt = 0*/
 }
 
-static void SetExitFlagSH(int sig)
+static void SetExitFlagSigHandler(int sig)
 {
     /*atomic store g_should_exit = 1*/
 }
@@ -248,8 +268,8 @@ static void SetExitFlagSH(int sig)
 static void RegisterSignalHandlersIMP(void)
 {
     /*sigemptyset*/
-    /*sigaction SIGUSR1 -> ResetCounterSH*/
-    /*sigaction SIGUSR2 -> SetExitFlagSH*/
+    /*sigaction SIGUSR1 -> ResetCounterSigHandler*/
+    /*sigaction SIGUSR2 -> SetExitFlagSigHandler*/
 }
 
 static void TaskCleanupIMP(void* unused)
