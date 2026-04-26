9de02056dd477b9f7c847e1d38d54f12d0a7029b
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-04-26 12:32:58 +0300

    wd.c : mmi function psudo code intial round

---
+++ system_programming/src/wd.c
@@ -0,0 +1,26 @@
+
+
+
+/*-----------------------------------------------------------------------------*/
+int MakeMeImmortal(size_t cmd_len, const char** cmd, int how_often, int fail_cnt)
+{
+	/*declare on a thread */
+	/*declare on an unamed semaphore (IPC between client and thread)*/
+	/*declare on int var to track status*/
+	/*declare on a thread arguments struct*/
+	
+	/*asserts*/
+	/*set thread arguments struct*/
+	/*create thread and assign it to WdThread funtion and store return value 
+	  in status*/
+		/*if return status is failed */
+			/*return failed*/
+		/*otherwise*/
+			/*wait till you recieve the semaphore*/
+	/*return success, wd mechanism launched successfully*/
+}
+/*-----------------------------------------------------------------------------*/
+void DoNotResuscitate(void)
+{
+
+}
