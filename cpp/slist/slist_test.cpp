1b23c2c77b1e9c707b8ba57faaf844095e910902
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-05-11 14:11:03 +0300

    slist_test.cpp : unit testing for slist cpp

--- cpp/slist/slist_test.cpp
+++ cpp/slist/slist_test.cpp
@@ -1,18 +1,59 @@
 #include <iostream>
 
 #include "slist.hpp"
+/* forward declaration tests */
+void TestEmptyList();
+void TestInsertion();
+void TestPopFront();
 
 int main()
 {
-    hrd42::Slist slist;
-    std::cout << "IsEmpty: " << slist.IsEmpty() << std::endl;
-    slist.PushFront(1);
-    slist.PushFront(2);
-    slist.PushFront(3);
-    std::cout << "Size: " << slist.Size() << std::endl;
-    std::cout << "IsEmpty: " << slist.IsEmpty() << std::endl;
-    std::cout << "PopFront: " << slist.PopFront() << std::endl;
-    std::cout << "Size: " << slist.Size() << std::endl;
+    TestEmptyList();
+    TestInsertion();
+    TestPopFront();
     return 0;
 }
 
+void TestEmptyList()
+{
+    hrd42::Slist slist;
+    bool expected_ = true;
+    if(slist.IsEmpty() != expected_)
+    {
+        std::cout << "TestEmptyList failed" << std::endl;
+    }
+}
+void TestInsertion()
+{
+    hrd42::Slist slist;
+    slist.PushFront(1);
+    slist.PushFront(2);
+    slist.PushFront(3);
+    size_t expected_size = 3;
+    if(slist.Size() != expected_size)
+    {
+        std::cout << "TestInsertion failed: size" << std::endl;
+    }
+    if(slist.IsEmpty())
+    {
+        std::cout << "TestInsertion failed: is empty" << std::endl;
+    }
+}
+
+void TestPopFront()
+{
+    hrd42::Slist slist;
+    slist.PushFront(1);
+    slist.PushFront(2);
+    slist.PushFront(3);
+    int expected_val = 3;
+    if(slist.PopFront() != expected_val)
+    {
+        std::cout << "TestPopFront failed: value" << std::endl;
+    }
+    size_t expected_size = 2;
+    if(slist.Size() != expected_size)
+    {
+        std::cout << "TestPopFront failed: size" << std::endl;      
+    }
+}
