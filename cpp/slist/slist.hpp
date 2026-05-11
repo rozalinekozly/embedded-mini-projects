aa9907f4853772a42a7b9309fdd317878ec4146d
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-05-11 09:41:59 +0300

    first version of slist header file (before review)

---
+++ cpp/slist/slist.hpp
@@ -0,0 +1,41 @@
+#ifndef __SLIST_HPP__
+#define __SLIST_HPP__
+
+#include <cstddef> //size_t
+
+namespace hrd42
+{
+    
+class Slist
+{
+    public: //public access modifier: accessible from anywhere 
+            //inside, outside class or other files 
+
+        //special public member functions
+        explicit Slist(); //default ctor, creates an empty list instance
+        ~Slist(); //dtor
+
+        //public member functions
+        void PushFront(int val_); 
+        int PopFront();
+        size_t Size() const; 
+        bool IsEmpty() const;
+
+        //instruction to the compiler not to create these special functions by default
+        Slist(const Slist& other_) = delete;
+        Slist& operator=(const Slist& other_) = delete;
+
+    private: //private access modifier: accessible only inside class itself 
+            //(memeber methods) + hide data from outside 
+        //private data members
+        class Node;     //restricting that class's visisbility 
+                        //exclusively to the containing class
+        Node* m_start;  //start of the slist
+
+};
+
+}// namespace hrd42
+
+#endif //__SLIST_HPP__
+
+
