98df87d19d2ae60cf51b0b152274391144106836
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-05-13 17:02:52 +0300

    complex_test.cpp : testing operators == and != in addditon to setters and getters

--- cpp/complex/complex_test.cpp
+++ cpp/complex/complex_test.cpp
@@ -11,6 +11,8 @@ int main()
     cout << "testing ctor" << endl;
     cout << "c1: " << c1 << endl;
     cout << "c2: " << c2 << endl;
+
+    
     
     std::cout << "testing operator +=" << std::endl;
     c1 += c2; // 4 + 6i
@@ -27,6 +29,22 @@ int main()
     std::cout << "testing operator /=" << std::endl;
     c1 /= c2; // (-5*3 + 10*4) + (-5*4 - 10*3)i / (3^2 + 4^2) = 1 + 2i
     cout << "c1 /= c2 =  " << c1 << endl;
+
+    cout << "testing operator ==" << endl;
+    cout << "c1 " << c1 << " == c2 " << c2 << ": " << (c1 == c2) << endl;
+    
+    cout << "testing operator !=" << endl;
+    cout << "c1 " << c1 << " != c2 " << c2 << ": " << (c1 != c2) << endl;
+
+    cout << "testing setters" << endl;
+    c1.SetReal(5);
+    c1.SetImg(6);
+    cout << "c1 after setting real to 5 and img to 6: " << c1 << endl;
+
+    cout << "testing getters" << endl;  
+    cout << "c1 real part: " << c1.GetReal() << endl;
+    cout << "c1 img part: " << c1.GetImg() << endl;
+    
     
     return 0;
 }
