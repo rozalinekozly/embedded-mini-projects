39c3f823b8bac900ea05f267d503e36b326b2e7a
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-05-13 17:34:12 +0300

    complex_test.cpp : organized test to test specific properties

--- cpp/complex/complex_test.cpp
+++ cpp/complex/complex_test.cpp
@@ -3,18 +3,65 @@
 
 using namespace hrd42;
 using namespace std;
+
+void TestPrinting();
+void TestSymmetricOps();
+void TestCtors();
+void TestNonSymmetricOPs();
+void TestSettersGetters();
+
+
 int main()
 {
-    Complex c1(1, 2); //1 + 2i
-    Complex c2(3, 4);  // 3 + 4i
-    
-    cout << "testing ctor" << endl;
-    cout << "c1: " << c1 << endl;
-    cout << "c2: " << c2 << endl;
 
+    TestPrinting();
+
+    TestCtors();
+
+    TestSettersGetters();
+
+    TestNonSymmetricOPs();
     
-    
+    TestSymmetricOps();
+
+    return 0;
+}
+
+
+void TestCtors()
+{
+    cout << "testing ctor given real and img arguments:" << endl;
+    Complex c1(1, 2); //1 + 2i
+    cout << "c1: " << c1 << endl<<endl;
+
+    cout << "testing ctor given no arguments:" << endl;
+    Complex c2;  // 0 + 0i
+    cout << "c2: " << c2 << endl<<endl;
+
+    cout << "testing ctor given only real argument:" << endl;
+    Complex c3(3); // 3 + 0i
+    cout << "c3: " << c3 << endl<<endl;
+}
+
+void TestSettersGetters()
+{
+    Complex c1(1, 2);   
+
+    cout << "testing setters" << endl;
+    c1.SetReal(5);
+    c1.SetImg(6);
+    cout << "c1 after setting real to 5 and img to 6: " << c1 << endl;
+
+    cout << "testing getters" << endl;  
+    cout << "c1 real part: " << c1.GetReal() << endl;
+    cout << "c1 img part: " << c1.GetImg() << endl;
+}
+
+void TestNonSymmetricOPs()
+{
     std::cout << "testing operator +=" << std::endl;
+    Complex c1(1, 2);
+    Complex c2(3, 4);
     c1 += c2; // 4 + 6i
     cout << "c1 += c2 =  " << c1 << endl;
     
@@ -35,16 +82,32 @@ int main()
     
     cout << "testing operator !=" << endl;
     cout << "c1 " << c1 << " != c2 " << c2 << ": " << (c1 != c2) << endl;
+}
 
-    cout << "testing setters" << endl;
-    c1.SetReal(5);
-    c1.SetImg(6);
-    cout << "c1 after setting real to 5 and img to 6: " << c1 << endl;
+void TestPrinting()
+{
+    cout << "testing operator <<" << endl;
+    Complex c1(1, 2);
+    cout << "c1: " << c1 << endl;
+}
+void TestSymmetricOps()
+{
+    Complex c1(5, 6);
+    Complex c2(3, 4);
 
-    cout << "testing getters" << endl;  
-    cout << "c1 real part: " << c1.GetReal() << endl;
-    cout << "c1 img part: " << c1.GetImg() << endl;
-    
-    
-    return 0;
+    cout << "testing operator +" << endl;
+    Complex c3 = c1 + c2; 
+    cout << "c1 + c2 = " << c1 << " + " << c2 << " = " << c3 << endl <<endl;
+
+    cout << "testing operator -" << endl;
+    Complex c4 = c1 - c2; 
+    cout << "c1 - c2 = " << c1 << " - " << c2 << " = " << c4 << endl << endl;   
+
+    cout << "testing operator *" << endl;
+    Complex c5 = c1 * c2; 
+    cout << "c1 * c2 = " << c1 << " * " << c2 << " = " << c5 << endl << endl;
+
+    cout << "testing operator /" << endl;
+    Complex c6 = c1 / c2; 
+    cout << "c1 / c2 = " << c1 << " / " << c2 << " = " << c6 << endl << endl;
 }
