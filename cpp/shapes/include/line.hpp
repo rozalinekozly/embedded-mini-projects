f24b2d4f47feee6e986ca9228e405d973c83bc9c
Rozaline Kozly <rozaline.kozly@infinitylabs.com>
2026-05-25 15:48:37 +0300

    line.hpp: adding another shape's header

---
+++ cpp/shapes/include/line.hpp
@@ -0,0 +1,29 @@
+// Based on composite design pattern of Shape
+// line 
+
+#ifndef ILRD_HRD42_LINE_HPP
+#define ILRD_HRD42_LINE_HPP
+
+#include "shape.hpp" // Shape API
+
+namespace hrd42
+{
+class Line : public Shape // non copyable
+{
+public:
+    explicit Line(const ilrd::Point& center,
+                  double length = 1,
+                  double angle = 0,
+                  COLORS color = COLOR_WHITE);
+    // using generated dtor
+
+    virtual Line& Draw() const;
+    virtual Line* Clone() const;
+
+private:
+    double m_length;
+};
+
+} // namespace hrd42
+
+#endif // ILRD_HRD42_LINE_HPP
